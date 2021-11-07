11771e07e4b9bd34ea3843a68c47b8cd56932838
HitJatin <adriano23012001@gmail.com>
2021-11-07 22:47:44 +0530

    added basic structure for blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+//Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import ArticlesList from './pages/ArticlesList';
+import Article from './pages/Article';
+
+// Components
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className='max-w-screen-md mx-auto pt-20'>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/articles-list" element={<ArticlesList />} />
+        <Route exact path="/article/:name" element={<Article />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+                About
+            </h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Welcome to my blog! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus suscipit repellat, modi, iure laborum ea blanditiis architecto laboriosam nesciunt rem mollitia nihil commodi? Reiciendis doloribus ratione eligendi sint, totam impedit omnis laborum! Qui debitis numquam voluptatem magnam tempore. Sed, est! Iste labore consequuntur, esse repellat molestiae alias fuga veniam aliquid?
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores labore, qui libero at doloribus dicta quaerat ab. Libero, optio natus. Explicabo officia blanditiis a possimus vero iure, doloribus itaque recusandae, corrupti fugiat aspernatur eligendi fugit minima ducimus omnis! Accusamus, labore quod adipisci quia fugiat fugit ratione et laudantium deleniti eum?
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. At error saepe exercitationem, dicta voluptas tenetur nulla possimus eum quidem modi sint necessitatibus, voluptates quo vitae perspiciatis enim sunt, culpa quae architecto voluptate odit? Quis sed aperiam magnam quas, ipsa necessitatibus cupiditate vero corporis dolorum quasi mollitia quia atque ea molestiae at a eum quae pariatur saepe dolore aspernatur. Reiciendis omnis deleniti quia culpa a eligendi voluptatibus sit, pariatur soluta, fuga ad eaque ullam aliquid perferendis. Nemo, laudantium veritatis? Est commodi tempore iure magnam, vel dolore quidem aperiam et architecto laborum repellat optio eum consectetur enim velit qui unde quisquam sint.
+            </p>
+        </div>
+    )
+}
+
+export default About
