bc893e2829ce7ddbfe1fe10a460d8c88a98d60e8
HitJatin <adriano23012001@gmail.com>
2021-11-08 11:38:57 +0530

    completed first version

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import ArticlesList from './pages/ArticlesList';
 import Article from './pages/Article';
+import NotFound from './pages/NotFound';
 
 // Components
 import NavBar from './components/NavBar';
@@ -14,10 +15,11 @@ function App() {
       <NavBar />
       <div className='max-w-screen-md mx-auto pt-20'>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/articles-list" element={<ArticlesList />} />
-        <Route exact path="/article/:name" element={<Article />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/articles-list" element={<ArticlesList />} />
+        <Route path="/article/:name" element={<Article />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       </div>
     </Router>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router';
+import Articles from '../components/Articles';
 import articleContent from './article-content';
+import NotFound from './NotFound';
 
 const Article = () => {
     const params = useParams();
     const name = params.name;
     const article = articleContent.find((article) => article.name === name);
-    if(!article) return <h1>Article does not exist</h1>;
+    const otherArticles = articleContent.filter((article) => article.name!== name);
+    if(!article) return <NotFound />;
     return (
         <>
             <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
@@ -17,6 +20,12 @@ const Article = () => {
                     {paragraph}
                 </p>
             ))}
+            <h1 className='sm:text-2x text-xl font-bold mt-4 mb-4 text-gray-900'>
+                Other Articles
+            </h1>
+            <div className="flex flex-wrap -m-4">
+                <Articles articles={otherArticles} />
+            </div>
         </>
     )
 }
